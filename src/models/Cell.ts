8e3999210f30472a83351a4278b44709cc2187c7
YuriiChertkov <yuriichertkov@gmail.com>
2022-05-19 13:35:58 +0300

    programming king queen bishop rook moving

--- src/models/Cell.ts
+++ src/models/Cell.ts
@@ -1,32 +1,84 @@
-import { Board } from './Board';
-import { Colors } from './Colors';
-import { Figure } from './figures/Figure';
+import { Board } from "./Board";
+import { Colors } from "./Colors";
+import { Figure } from "./figures/Figure";
 export class Cell {
-    readonly x: number;
-    readonly y: number;
-    readonly color: Colors;
-    figure: Figure | null;
-    board: Board;
-    available: boolean;
-    id: number;
-    
-    constructor(board: Board, x: number, y: number, color: Colors, figure: Figure |null ){
-        this.x = x;
-        this.y = y;
-        this.color = color;
-        this.figure = figure;
-        this.board = board;
-        this.available = false;
-        this.id = Math.random();
+  readonly x: number;
+  readonly y: number;
+  readonly color: Colors;
+  figure: Figure | null;
+  board: Board;
+  available: boolean;
+  id: number;
 
-    }
+  constructor(
+    board: Board,
+    x: number,
+    y: number,
+    color: Colors,
+    figure: Figure | null
+  ) {
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.figure = figure;
+    this.board = board;
+    this.available = false;
+    this.id = Math.random();
+  }
 
-    moveFigure(target: Cell){
-        if (this.figure?.canMove(target)) {
-            this.figure.moveFigure(target)
-            target.figure = this.figure;
-            this.figure = null;
-            
-        }
+  isEmpty() {
+    return this.figure === null;
+  }
+
+  isEmptyVertical(target: Cell): boolean {
+    if (this.x !== target.x) return false;
+
+    const min = Math.min(this.y, target.y);
+    const max = Math.max(this.y, target.y);
+    for (let i = min + 1; i < max; i++) {
+      if (!this.board.getCell(this.x, i).isEmpty()) {
+        return false;
+      }
     }
+    return true;
+  }
+  isEmptyHorizontal(target: Cell): boolean {
+    if (this.y !== target.y) return false;
+
+    const min = Math.min(this.x, target.x);
+    const max = Math.max(this.x, target.x);
+    for (let i = min + 1; i < max; i++) {
+      if (!this.board.getCell(i, this.y).isEmpty()) {
+        return false;
+      }
+    }
+    return true;
+  }
+  isEmptyDiagonal(target: Cell): boolean {
+    const absX = Math.abs(target.x - this.x);
+    const absY = Math.abs(target.y - this.y);
+    if (absX !== absY) return false;
+
+    const dy = this.y < target.y ? 1 : -1;
+    const dx = this.x < target.x ? 1 : -1;
+    for (let i = 1; i < absY; i++) {
+      if (!this.board.getCell(this.x + dx * i, this.y + dy * i).isEmpty()) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  setFigure(figure: Figure) {
+    this.figure = figure;
+    this.figure.cell = this;
+  }
+
+  moveFigure(target: Cell) {
+    if (this.figure?.canMove(target)) {
+      this.figure.moveFigure(target);
+      target.setFigure(this.figure);
+      this.figure = null;
+    }
+  }
 }
